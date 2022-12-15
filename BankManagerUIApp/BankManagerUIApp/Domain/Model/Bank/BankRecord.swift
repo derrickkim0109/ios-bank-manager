e72be2dd22b868ea375d204bb3c2f4c2c8795b43
derrickkim0109 <derrickkim0109@naver.com>
2022-12-15 23:06:34 +0900

    chore: 샘플 데이터 삭제

---
+++ BankManagerUIApp/BankManagerUIApp/Domain/Model/Bank/BankRecord.swift
@@ -0,0 +1,13 @@
+//
+//  BankRecord.swift
+//  BankManagerUIApp
+//
+//  Created by Derrick kim on 2022/12/14.
+//
+
+import Foundation
+
+struct BankRecord: Hashable {
+    let id = UUID()
+    let task: String
+}
