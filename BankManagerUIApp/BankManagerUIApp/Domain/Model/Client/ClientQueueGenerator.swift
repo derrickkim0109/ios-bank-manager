a1c4b217e58251c7d2991539615bb4b43e030c04
derrickkim0109 <derrickkim0109@naver.com>
2022-12-15 23:08:00 +0900

    refactor: ClientQueueGenerator setupList 기능 추가

--- BankManagerUIApp/BankManagerUIApp/Domain/Model/Client/ClientQueueGenerator.swift
+++ BankManagerUIApp/BankManagerUIApp/Domain/Model/Client/ClientQueueGenerator.swift
@@ -7,18 +7,26 @@
 
 import Foundation
 
-struct ClientQueueGenerator {
-    func generate(_ clientAmount: Int) -> ClientQueue<Client> {
-        var clientQueue = ClientQueue<Client>()
+final class ClientQueueGenerator {
+    private var clientsBankRecord: [BankRecord] = []
 
-        for number in 1...clientAmount {
+    func generate(_ clientAmount: Int) -> ClientQueue<Client> {
+        let clientQueue = ClientQueue<Client>()
+
+        for number in clientAmount..<clientAmount + 10 {
             guard let requestName = Request.allCases.randomElement() else {
                 return ClientQueue<Client>()
             }
+            let client = Client(request: requestName, waitingNumber: number)
+            clientsBankRecord.append(client.pickedUpTicket)
 
-            clientQueue.enqueue(Client(request: requestName, waitingNumber: number))
+            clientQueue.enqueue(client)
         }
 
         return clientQueue
+    }
+
+    func setupClientList() -> [BankRecord] {
+        return clientsBankRecord
     }
 }
