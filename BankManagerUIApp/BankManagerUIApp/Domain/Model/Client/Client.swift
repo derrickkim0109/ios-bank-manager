658eb7b2f5a5594906caf9c4d4194f7ccd6e0985
derrickkim0109 <derrickkim0109@naver.com>
2022-12-19 10:58:31 +0900

    feat: String으로 들어오는 값 처리하기 위한 프로퍼티 추가

--- BankManagerUIApp/BankManagerUIApp/Domain/Model/Client/Client.swift
+++ BankManagerUIApp/BankManagerUIApp/Domain/Model/Client/Client.swift
@@ -11,6 +11,10 @@ struct Client {
     private(set) var waitingNumber: Int
     private(set) var request: Request
 
+    var pickedUpTicket: BankRecord {
+        return BankRecord(task: "\(waitingNumber)-\(request.koreanTitle)")
+    }
+
     init(request: Request, waitingNumber: Int) {
         self.waitingNumber = waitingNumber
         self.request = request
